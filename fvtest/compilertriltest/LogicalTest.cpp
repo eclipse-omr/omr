af5aa2bda65d713f6efd0601c82b043fd0bfc917
Spencer Comin <spencer.comin@ibm.com>
2025-04-10 15:06:56 -0400

    Add tril tests for 8 & 16 bit binary logical opcodes
    Signed-off-by: Spencer Comin <spencer.comin@ibm.com>

--- fvtest/compilertriltest/LogicalTest.cpp
+++ fvtest/compilertriltest/LogicalTest.cpp
@@ -24,6 +24,21 @@
 #include "default_compiler.hpp"
 #include "omrformatconsts.h"
 
+template <typename uint_t>
+uint_t bitOR(uint_t l, uint_t r) {
+    return l | r;
+}
+
+template <typename uint_t>
+uint_t bitAND(uint_t l, uint_t r) {
+    return l & r;
+}
+
+template <typename uint_t>
+uint_t bitXOR(uint_t l, uint_t r) {
+    return l ^ r;
+}
+
 int16_t sbyteswap(int16_t l) {
     return ((l << 8) & 0xff00)
          | ((l >> 8) & 0x00ff);
@@ -33,11 +48,11 @@ int32_t ineg(int32_t l) {
     return -l;
 }
 
-int32_t iabs(int32_t l) { 
-   if (l >= 0) 
+int32_t iabs(int32_t l) {
+   if (l >= 0)
       return l;
-   else 
-      return -1 * l; 
+   else
+      return -1 * l;
 }
 
 int32_t ibyteswap(int32_t l) {
@@ -45,18 +60,6 @@ int32_t ibyteswap(int32_t l) {
          | ((l <<  8) & 0x00ff0000)
          | ((l >>  8) & 0x0000ff00)
          | ((l >> 24) & 0x000000ff);
-}
-
-int32_t ior(int32_t l, int32_t r) {
-    return l | r;
-}
-
-int32_t iand(int32_t l, int32_t r) {
-    return l & r;
-}
-
-int32_t ixor(int32_t l, int32_t r) {
-    return l ^ r;
 }
 
 int64_t lbyteswap(int64_t l) {
@@ -68,18 +71,6 @@ int64_t lbyteswap(int64_t l) {
          | ((l >> 24) & 0x0000000000ff0000)
          | ((l >> 40) & 0x000000000000ff00)
          | ((l >> 56) & 0x00000000000000ff);
-}
-
-int64_t lor(int64_t l, int64_t r) {
-    return l | r;
-}
-
-int64_t land(int64_t l, int64_t r) {
-    return l & r;
-}
-
-int64_t lxor(int64_t l, int64_t r) {
-    return l ^ r;
 }
 
 int64_t lneg(int64_t l) {
@@ -159,7 +150,7 @@ INSTANTIATE_TEST_CASE_P(LogicalTest, Int16LogicalUnary, ::testing::Combine(
         std::tuple<const char*, int16_t(*)(int16_t)>("sbyteswap", sbyteswap)
         )));
 
-class Int32LogicalUnary : public TRTest::UnaryOpTest<int32_t> {}; 
+class Int32LogicalUnary : public TRTest::UnaryOpTest<int32_t> {};
 
 TEST_P(Int32LogicalUnary, UsingConst) {
     auto param = TRTest::to_struct(GetParam());
@@ -216,115 +207,6 @@ INSTANTIATE_TEST_CASE_P(LogicalTest, Int32LogicalUnary, ::testing::Combine(
         std::tuple<const char*, int32_t(*)(int32_t)>("iabs", iabs)
     )));
 
-class Int32LogicalBinary : public TRTest::BinaryOpTest<int32_t> {};
-
-TEST_P(Int32LogicalBinary, UsingConst) {
-    auto param = TRTest::to_struct(GetParam());
-
-    char inputTrees[120] = {0};
-    std::snprintf(inputTrees, 120, "(method return=Int32 (block (ireturn (%s (iconst %d) (iconst %d)) )))", param.opcode.c_str(), param.lhs, param.rhs);
-    auto trees = parseString(inputTrees);
-
-    ASSERT_NOTNULL(trees);
-
-    Tril::DefaultCompiler compiler(trees);
-
-    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
-
-    auto entry_point = compiler.getEntryPoint<int32_t (*)(void)>();
-    volatile auto exp = param.oracle(param.lhs, param.rhs);
-    volatile auto act = entry_point();
-    ASSERT_EQ(exp, act);
-}
-
-TEST_P(Int32LogicalBinary, UsingLoadParam) {
-    auto param = TRTest::to_struct(GetParam());
-
-    char inputTrees[120] = {0};
-    std::snprintf(inputTrees, 120, "(method return=Int32 args=[Int32, Int32] (block (ireturn (%s (iload parm=0) (iload parm=1)) )))", param.opcode.c_str());
-
-    auto trees = parseString(inputTrees);
-
-    ASSERT_NOTNULL(trees);
-
-    Tril::DefaultCompiler compiler(trees);
-
-    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
-
-    auto entry_point = compiler.getEntryPoint<int32_t (*)(int32_t, int32_t)>();
-    ASSERT_EQ(param.oracle(param.lhs, param.rhs), entry_point(param.lhs, param.rhs));
-}
-
-INSTANTIATE_TEST_CASE_P(LogicalTest, Int32LogicalBinary, ::testing::Combine(
-    ::testing::ValuesIn(TRTest::const_value_pairs<int32_t,int32_t>()),
-    ::testing::Values(
-        std::tuple<const char*, int32_t(*)(int32_t, int32_t)>("ior", ior),
-        std::tuple<const char*, int32_t(*)(int32_t, int32_t)>("iand", iand),
-        std::tuple<const char*, int32_t(*)(int32_t, int32_t)>("ixor", ixor)
-    )));
-
-class Int64LogicalBinary : public TRTest::BinaryOpTest<int64_t> {};
-
-TEST_P(Int64LogicalBinary, UsingConst) {
-    auto param = TRTest::to_struct(GetParam());
-
-    char inputTrees[160] = {0};
-    std::snprintf(inputTrees, 160, 
-        "(method return=Int64"
-        "  (block"
-        "    (lreturn"
-        "      (%s"
-        "        (lconst %" OMR_PRId64 ")"
-        "        (lconst %" OMR_PRId64 ") ) ) ) )",
-        param.opcode.c_str(), param.lhs, param.rhs);
-    auto trees = parseString(inputTrees);
-
-    ASSERT_NOTNULL(trees);
-
-    Tril::DefaultCompiler compiler(trees);
-
-    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
-
-    auto entry_point = compiler.getEntryPoint<int64_t (*)(void)>();
-    volatile auto exp = param.oracle(param.lhs, param.rhs);
-    volatile auto act = entry_point();
-    ASSERT_EQ(exp, act);
-}
-
-TEST_P(Int64LogicalBinary, UsingLoadParam) {
-    auto param = TRTest::to_struct(GetParam());
-
-    char inputTrees[160] = {0};
-    std::snprintf(inputTrees, 160, 
-        "(method return=Int64 args=[Int64, Int64]"
-        "  (block"
-        "    (lreturn"
-        "      (%s"
-        "        (lload parm=0)"
-        "        (lload parm=1) ) ) ) )", 
-        param.opcode.c_str());
-
-    auto trees = parseString(inputTrees);
-
-    ASSERT_NOTNULL(trees);
-
-    Tril::DefaultCompiler compiler(trees);
-
-    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
-
-    auto entry_point = compiler.getEntryPoint<int64_t (*)(int64_t, int64_t)>();
-    ASSERT_EQ(param.oracle(param.lhs, param.rhs), entry_point(param.lhs, param.rhs));
-}
-
-INSTANTIATE_TEST_CASE_P(LogicalTest, Int64LogicalBinary, ::testing::Combine(
-	::testing::ValuesIn(TRTest::const_value_pairs<int64_t,int64_t>()),
-	::testing::Values(
-		std::tuple<const char*, int64_t(*)(int64_t, int64_t)>("lor", lor),
-		std::tuple<const char*, int64_t(*)(int64_t, int64_t)>("land", land),
-		std::tuple<const char*, int64_t(*)(int64_t, int64_t)>("lxor", lxor)
-   )));
-
-                
 class Int64LogicalUnary : public TRTest::UnaryOpTest<int64_t> {};
 
 TEST_P(Int64LogicalUnary, UsingConst) {
@@ -396,3 +278,206 @@ INSTANTIATE_TEST_CASE_P(LogicalTest, Int64LogicalUnary, ::testing::Combine(
         std::tuple<const char*, int64_t(*)(int64_t)>("lbyteswap", lbyteswap),
         std::tuple<const char*, int64_t(*)(int64_t)>("lneg", lneg)
         )));
+
+
+class Int8LogicalBinary : public TRTest::BinaryOpTest<uint8_t> {};
+
+TEST_P(Int8LogicalBinary, UsingConst) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int8 (block (ireturn (%s (bconst %d) (bconst %d)))))", param.opcode.c_str(), param.lhs, param.rhs);
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint8_t (*)(void)>();
+    volatile auto exp = param.oracle(param.lhs, param.rhs);
+    volatile auto act = entry_point();
+    ASSERT_EQ(exp, act);
+}
+
+TEST_P(Int8LogicalBinary, UsingLoadParam) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int8 args=[Int8, Int8] (block (ireturn (%s (bload parm=0) (bload parm=1)))))", param.opcode.c_str());
+
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint8_t (*)(uint8_t, uint8_t)>();
+    ASSERT_EQ(param.oracle(param.lhs, param.rhs), entry_point(param.lhs, param.rhs));
+}
+
+INSTANTIATE_TEST_CASE_P(LogicalTest, Int8LogicalBinary, ::testing::Combine(
+    ::testing::ValuesIn(TRTest::const_value_pairs<uint8_t,uint8_t>()),
+    ::testing::Values(
+        std::tuple<const char*, uint8_t(*)(uint8_t, uint8_t)>("bor", bitOR),
+        std::tuple<const char*, uint8_t(*)(uint8_t, uint8_t)>("band", bitAND),
+        std::tuple<const char*, uint8_t(*)(uint8_t, uint8_t)>("bxor", bitXOR)
+    )));
+
+class Int16LogicalBinary : public TRTest::BinaryOpTest<uint16_t> {};
+
+TEST_P(Int16LogicalBinary, UsingConst) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int16 (block (ireturn (%s (sconst %d) (sconst %d)))))", param.opcode.c_str(), param.lhs, param.rhs);
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint16_t (*)(void)>();
+    volatile auto exp = param.oracle(param.lhs, param.rhs);
+    volatile auto act = entry_point();
+    ASSERT_EQ(exp, act);
+}
+
+TEST_P(Int16LogicalBinary, UsingLoadParam) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int16 args=[Int16, Int16] (block (ireturn (%s (sload parm=0) (sload parm=1)))))", param.opcode.c_str());
+
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint16_t (*)(uint16_t, uint16_t)>();
+    ASSERT_EQ(param.oracle(param.lhs, param.rhs), entry_point(param.lhs, param.rhs));
+}
+
+INSTANTIATE_TEST_CASE_P(LogicalTest, Int16LogicalBinary, ::testing::Combine(
+    ::testing::ValuesIn(TRTest::const_value_pairs<uint16_t,uint16_t>()),
+    ::testing::Values(
+        std::tuple<const char*, uint16_t(*)(uint16_t, uint16_t)>("sor", bitOR),
+        std::tuple<const char*, uint16_t(*)(uint16_t, uint16_t)>("sand", bitAND),
+        std::tuple<const char*, uint16_t(*)(uint16_t, uint16_t)>("sxor", bitXOR)
+    )));
+
+class Int32LogicalBinary : public TRTest::BinaryOpTest<uint32_t> {};
+
+TEST_P(Int32LogicalBinary, UsingConst) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int32 (block (ireturn (%s (iconst %d) (iconst %d)) )))", param.opcode.c_str(), param.lhs, param.rhs);
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint32_t (*)(void)>();
+    volatile auto exp = param.oracle(param.lhs, param.rhs);
+    volatile auto act = entry_point();
+    ASSERT_EQ(exp, act);
+}
+
+TEST_P(Int32LogicalBinary, UsingLoadParam) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[120] = {0};
+    std::snprintf(inputTrees, 120, "(method return=Int32 args=[Int32, Int32] (block (ireturn (%s (iload parm=0) (iload parm=1)) )))", param.opcode.c_str());
+
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint32_t (*)(uint32_t, uint32_t)>();
+    ASSERT_EQ(param.oracle(param.lhs, param.rhs), entry_point(param.lhs, param.rhs));
+}
+
+INSTANTIATE_TEST_CASE_P(LogicalTest, Int32LogicalBinary, ::testing::Combine(
+    ::testing::ValuesIn(TRTest::const_value_pairs<uint32_t,uint32_t>()),
+    ::testing::Values(
+        std::tuple<const char*, uint32_t(*)(uint32_t, uint32_t)>("ior", bitOR),
+        std::tuple<const char*, uint32_t(*)(uint32_t, uint32_t)>("iand", bitAND),
+        std::tuple<const char*, uint32_t(*)(uint32_t, uint32_t)>("ixor", bitXOR)
+    )));
+
+class Int64LogicalBinary : public TRTest::BinaryOpTest<uint64_t> {};
+
+TEST_P(Int64LogicalBinary, UsingConst) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[160] = {0};
+    std::snprintf(inputTrees, 160,
+        "(method return=Int64"
+        "  (block"
+        "    (lreturn"
+        "      (%s"
+        "        (lconst %" OMR_PRId64 ")"
+        "        (lconst %" OMR_PRId64 ") ) ) ) )",
+        param.opcode.c_str(), param.lhs, param.rhs);
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint64_t (*)(void)>();
+    volatile auto exp = param.oracle(param.lhs, param.rhs);
+    volatile auto act = entry_point();
+    ASSERT_EQ(exp, act);
+}
+
+TEST_P(Int64LogicalBinary, UsingLoadParam) {
+    auto param = TRTest::to_struct(GetParam());
+
+    char inputTrees[160] = {0};
+    std::snprintf(inputTrees, 160,
+        "(method return=Int64 args=[Int64, Int64]"
+        "  (block"
+        "    (lreturn"
+        "      (%s"
+        "        (lload parm=0)"
+        "        (lload parm=1) ) ) ) )",
+        param.opcode.c_str());
+
+    auto trees = parseString(inputTrees);
+
+    ASSERT_NOTNULL(trees);
+
+    Tril::DefaultCompiler compiler(trees);
+
+    ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
+
+    auto entry_point = compiler.getEntryPoint<uint64_t (*)(uint64_t, uint64_t)>();
+    ASSERT_EQ(param.oracle(param.lhs, param.rhs), entry_point(param.lhs, param.rhs));
+}
+
+INSTANTIATE_TEST_CASE_P(LogicalTest, Int64LogicalBinary, ::testing::Combine(
+	::testing::ValuesIn(TRTest::const_value_pairs<uint64_t,uint64_t>()),
+	::testing::Values(
+		std::tuple<const char*, uint64_t(*)(uint64_t, uint64_t)>("lor", bitOR),
+		std::tuple<const char*, uint64_t(*)(uint64_t, uint64_t)>("land", bitAND),
+		std::tuple<const char*, uint64_t(*)(uint64_t, uint64_t)>("lxor", bitXOR)
+   )));
