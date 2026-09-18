7389b2afb8cb1dab05a564f72d51836fcfe5d188
AustinWells <austin.wells@mail.utoronto.ca>
2026-09-17 21:46:38 -0400

    Fixes clang-format check not liking the ternary split across 2 lines

--- compiler/control/OMROptions.cpp
+++ compiler/control/OMROptions.cpp
@@ -3121,8 +3121,7 @@ bool OMR::Options::jitLatePostProcess(TR::OptionSet *optionSet, void *jitConfig)
 
     // getFixedOptLevel() asserts command line, optionSet is NULL for command line options
     // so we can use it to determine if the opt level was set via the command line or not
-    int32_t inhibitOptLevel = optionSet ? self()->getOptLevel()
-                                        : self()->getFixedOptLevel();
+    int32_t inhibitOptLevel = optionSet ? self()->getOptLevel() : self()->getFixedOptLevel();
     if (inhibitOptLevel == -1 && self()->getOption(TR_InhibitRecompilation)) {
         self()->setOption(TR_DisableUpgradingColdCompilations);
         self()->setOption(TR_DisableGuardedCountingRecompilations);
